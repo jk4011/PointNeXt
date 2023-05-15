0d2642f41f8205c35f6828655cb229d53910873c
JinHyeok <jinhyuk@unist.ac.kr>
2023-05-15 10:23:53 +0000

    python module

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="pointnext",
+    version="0.0.1",
+    description="pointnext",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jk4011/PointNeXt",
+    author="Jinhyeok Kim",
+    author_email="jinhyuk@unist.ac.kr",
+    packages=["pointnext"],
+    python_requires=">=3.7, <4",
+    project_urls={
+        "Source": "https://github.com/jk4011/jhutil",
+    },
+)
